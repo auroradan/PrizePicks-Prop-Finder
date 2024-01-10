5341f56f963bbd2778b50f5968a2ad25109a4433
Dan Tran <103843222+auroradan@users.noreply.github.com>
2024-01-10 01:41:47 -0500

    Delete PropFinders directory

--- PropFinders/PropFinder.py
+++
@@ -1,9 +0,0 @@
-from NBAPropFinder import NBAPropFinder
-from PropFinders.NFLPropFinder import NFLPropFinder
-from PropFinders.NHLPropFinder import NHLPropFinder
-from PropFinders.NCAABPropFinder import NCAABPropFinder
-
-#ncaab_props = NCAABPropFinder()
-#nba_props = NBAPropFinder()
-# nhl_props = NHLPropFinder()
-#nfl_props = NFLPropFinder()

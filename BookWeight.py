0ce2c2f8771cb99b1a56084619c75f955c8e98c0
Dan Tran <103843222+auroradan@users.noreply.github.com>
2024-06-11 17:53:59 -0400

    Add files via upload

--- BookWeight.py
+++ BookWeight.py
@@ -1,7 +1,7 @@
 class BookWeight:
     def __init__(self):
         self.book_to_weight = {"FanDuel":34.04, "Caesars":7.67, "BetMGM":12.37, "DraftKings":17.18, "BetRivers":2.01}
-        self.private_books = set(["MyBookie.ag", "BetOnline.ag", "Bovada"])
+        self.private_books = set(["MyBookie.ag", "BetOnline.ag", "Bovada", "SuperBook"])
     
     def getBookToWeight(self):
         return self.book_to_weight
